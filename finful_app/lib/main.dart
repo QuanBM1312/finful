e8af02b2c62f5f672a871feac63aa32f3551d1b2
Duy <duypq2610@gmail.com>
2025-11-23 19:29:54 +0700

    refresh add

--- finful_app/lib/main.dart
+++ finful_app/lib/main.dart
@@ -1,10 +1,8 @@
-
-import 'dart:async';
-
 import 'package:finful_app/app/presentation/blocs/app_bloc_observe.dart';
 import 'package:finful_app/app/presentation/journey/app/root_app.dart';
 import 'package:finful_app/core/logger/finful_logger_level.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/injection/injection.dart';
@@ -12,6 +10,10 @@ import 'core/core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   Bloc.observer = AppBlocObserve();
   logger().initialize(FinfulLoggerLevel.debug);
 
